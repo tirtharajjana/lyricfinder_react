d14b00f23238ab3c4c5085b1b2a4d26005d90e9f
Tirtharaj <tirtharajjana615@gmail.com>
2021-07-15 22:14:47 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import './App.css';
+
+import Navbar from './components/layouts/Navbar';
+import Index from './components/layouts/Index';
+
+function App() {
+  return (
+    <Router>
+      <React.Fragment>
+        <Navbar />
+
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Index} />
+          </Switch>
+        </div>
+
+      </React.Fragment>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/tracks/Tracks.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class Tracks extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Track</h1>
+            </div>
+        )
+    }
+}
+
+export default Tracks
+
